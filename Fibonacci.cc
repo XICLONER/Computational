5fc6c2ac47983496979a7562045f4918130b4d7e
Ali Moradi Alamdarloo <timndus@gmail.com>
2023-01-13 12:39:54 +0330

    change fibonacci code style

--- Fibonacci.cc
+++ Fibonacci.cc
@@ -1,18 +1,23 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
     int p1, p2, p3;
+
     p1 = 1;
     p2 = 1;
-    cout << p1 << "\n"
-         << p2 << "\n";
-    for (int i = 0; i < 8; i++)
-    {
+
+    cout << p1 << "\n" << p2 << "\n";
+
+    for (int i = 0; i < 8; i++) {
         cout << p1 + p2 << "\n";
+        
         p3 = p1 + p2;
         p1 = p2;
         p2 = p3;
     }
+
     return 0;
 }
