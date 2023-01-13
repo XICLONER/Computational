d8fafe991f081f78fc50414a09a605cf6523f44a
Ali Moradi Alamdarloo <timndus@gmail.com>
2023-01-13 13:01:19 +0330

    dynamic input

--- Fibonacci.cc
+++ Fibonacci.cc
@@ -6,12 +6,24 @@ int main()
 {
     int p1, p2, p3;
 
+    // how many numbers of Fibonacci we want to print
+    short int n;
+
+    cout << "How many numbers of Fibonacci do you want to pring? (between 0 and 32)\n";
+    cin >> n;
+    cout << endl;
+
+    if (n < 0 || n > 32) {
+        cout << "Invalid input\n";
+        return 0;
+    }
+
     p1 = 1;
     p2 = 1;
 
     cout << p1 << "\n" << p2 << "\n";
 
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < n - 2; i++) {
         cout << p1 + p2 << "\n";
         
         p3 = p1 + p2;
