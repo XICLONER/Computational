9122e0547213332f0fe9a0c72eed1599021ba178
Ida <iamidaazad@gmail.com>
2024-06-01 10:28:18 +0330

    use functions for being able to counting spaces

--- length-of-a-string.cc
+++ length-of-a-string.cc
@@ -1,14 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 int main()
 {
     string text;
-    int count = 0;
-    cout << "Please enter your text: \n";
-    cin >> text;
-    for (int i = 0; text[i]; i++)
-    {
-        count ++;
-    }
-    cout << count << "\n";
+    cout << "Please enter your text: ";
+    getline(cin, text);
+    int length = text.length();
+    cout << "The length of the string is: " << length << endl;
+    return 0;
 }
